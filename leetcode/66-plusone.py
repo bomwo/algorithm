e640a1bb9ac8fbbd28a3e453900c5f278238d0a2
bomwo <bomwo25@gmail.com>
2022-03-05 21:07:26 +0900

    add 66 plus one solution

---
+++ leetcode/66-plusone.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        s = str()
+        for value in digits:
+            s += str(value)
+        s = int(s) + 1
+        return [a for a in str(s)]
+
+# 근데 타입을 바꾸면서 하는건 좋지 않을 것 같은데 나중에 다시 풀기
+# 일단 코드는 성공했다.
+
+
+if __name__ == "__main__":
+    test = Solution()
+    print(test.plusOne([9]))
